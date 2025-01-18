820f5600e43613329840b4ca1416cec2a7811ec4
sniper76 <snipers76@gmail.com>
2025-01-18 19:15:14 +0900

    init clear

--- upbit/bar_chart_data.py
+++ upbit/bar_chart_data.py
@@ -14,7 +14,7 @@ class BarChartData:
     def get_price_minute3(self):
         # 1. 빗썸 차트 데이터 가져오기 (3분봉)
         df = pyupbit.get_ohlcv("KRW-BTC", interval="minute3", count=10)
-        self.loggerObj.debug_method(df[["open", "high", "close", "volumn", "value"]])
+        self.loggerObj.debug_method(df[["open", "low", "high", "close", "volume"]])
         return df
 
 # Example usage
